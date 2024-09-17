7c7e3d995101b277e78b1cc068c41a95b89e8b74
Talha-Shaikh1 <164675183+Talha-Shaikh1@users.noreply.github.com>
2024-09-17 04:54:24 +0000

    update

--- 02_Basics/03_object.js
+++ 02_Basics/03_object.js
@@ -1,6 +1,12 @@
 const obj = new Object();
 // console.log(obj);
 
+const uberUser = new Object();
+
+uberUser.email = "Talha@gmai.com"
+uberUser.name = "Talha Shaikh"
+uberUser.isLoggedIn = true
+
 const appUser = {
     email: "Talha@gmail.com",
     fullName: {
@@ -11,4 +17,39 @@ const appUser = {
     }
 }
 
-console.log(appUser.fullName.Username.firstName);
+// console.log(appUser.fullName.Username.firstName);
+
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}
+
+const obj3 = Object.assign({}, obj1 , obj2);
+// console.log(obj3);
+const obj4 = {...obj1, ...obj3}
+// console.log(obj4);
+
+const user = [
+    {
+        id: 1,
+        email: "email@gmail.com"
+    },
+    {
+        id: 1,
+        email: "email@gmail.com"
+    },{
+        id: 1,
+        email: "email@gmail.com"
+    },{
+        id: 1,
+        email: "email@gmail.com"
+    },
+]
+
+console.log(user[1].email);
+console.log(uberUser);
+console.log(Object.keys(uberUser))
+console.log(Object.values(uberUser));
+console.log(Object.entries(uberUser));
+console.log(uberUser.hasOwnProperty('name'));
+console.log(uberUser.hasOwnProperty('id'));
+
