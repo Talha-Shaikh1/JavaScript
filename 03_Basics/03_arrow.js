341640b543645b29df8b9f9f8b4411a1c2d100db
Talha-Shaikh1 <164675183+Talha-Shaikh1@users.noreply.github.com>
2024-09-18 17:38:00 +0000

    update

---
+++ 03_Basics/03_arrow.js
@@ -0,0 +1,52 @@
+const user = {
+    userName: "Talha",
+    price: 1000,
+    welcomeMessage: function(){
+        console.log(`${this.userName}, welcome to our website`);
+        console.log(this);
+        
+    }
+}
+user.welcomeMessage()
+console.log(this);
+user.userName = "umer"
+user.welcomeMessage()
+
+
+// function chai(){
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const chai = function () {
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+const chai =  () => {
+    let username = "hitesh"
+    console.log(this);
+}
+
+
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) =>  num1 + num2
+
+// const addTwo = (num1, num2) => ( num1 + num2 )
+
+const addTwo = (num1, num2) => ({username: "hitesh"})
+
+
+console.log(addTwo(3, 4))
+
+
+// const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach()
