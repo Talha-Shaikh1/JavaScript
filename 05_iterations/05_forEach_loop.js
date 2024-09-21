371a26e2a024e35861bf32827707ae7c4ee7d896
Talha-Shaikh1 <164675183+Talha-Shaikh1@users.noreply.github.com>
2024-09-21 08:37:36 +0000

    update

---
+++ 05_iterations/05_forEach_loop.js
@@ -0,0 +1,57 @@
+const codingLanguages = ["js", "c++", "java", "python"];
+
+// codingLanguages.forEach(function(val){
+//     console.log(val,"coding languages");
+    
+// })
+
+
+
+
+// codingLanguages.forEach( (item) => {
+//     console.log(item);
+    
+// })
+
+
+// function printME(item){
+//     console.log(item);
+    
+// }
+
+// codingLanguages.forEach(printME)
+
+
+
+// codingLanguages.forEach( (item, index, arr) =>{
+    
+//     console.log(item, index, arr);
+    
+// } )
+
+
+
+codingLanguages.forEach((codingLanguages=>{
+    console.log(codingLanguages, "coding languages");
+    
+}))
+
+const myCodingLanguages = [
+    {
+        languageName: "Javascript",
+        fileName: "js"
+    },
+    {
+        languageName: "JAva",
+        fileName: "java"
+    },
+    {
+        languageName: "python",
+        fileName: "py"
+    }
+]
+
+myCodingLanguages.forEach((myCodingLanguages => {
+    console.log(myCodingLanguages.languageName);
+    
+}))
